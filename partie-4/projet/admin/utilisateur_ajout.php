7efb81fbefebccc5d3b8cd87e70f4d82a08cba3c
wizardy <wizardhassan99@gmail.com>
2023-10-19 21:30:56 +0100

    form : ajouter nv utilisateur

---
+++ partie-4/projet/admin/utilisateur_ajout.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ajouter nouvel utilisateur</title>
+</head>
+<body>
+  <h1>Ajouter nouvel utilisateur</h1>
+</body>
+</html>
