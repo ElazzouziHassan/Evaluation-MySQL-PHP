ff92b5e52123f0e57e65ecfe34a7a065e9d06005
wizardy <wizardhassan99@gmail.com>
2023-10-19 21:43:19 +0100

    ajouter utilisateur action

---
+++ partie-4/projet/admin/utilisateur_ajout_action.php
@@ -0,0 +1,17 @@
+<?php
+include("..\inc\connection.php");
+include("..\classes\utilisateur.class.php");
+
+$user = new Utilisateur();
+$user->setNom($_POST['nom']);
+$user->setPrenom($_POST['prenom']);
+$user->setMail($_POST['mail']);
+$user->setMDP($_POST['mdp']);
+$user->setDNaissance($_POST['date-naissance']);
+
+if ($user->enregistrer($bdd)) {
+    echo "Ajout utilisateur OK.";
+} else {
+    echo "Erreur lors de l'ajout de l'utilisateur.";
+}
+?>
