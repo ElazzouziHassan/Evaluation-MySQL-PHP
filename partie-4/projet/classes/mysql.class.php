0d9b213c7a2f0cb1afdab0520440a5e23fafda3f
wizardy <wizardhassan99@gmail.com>
2023-10-20 20:38:22 +0100

    mysql class

--- partie-4/projet/classes/mysql.class.php
+++ partie-4/projet/classes/mysql.class.php
@@ -1,56 +1,47 @@
 <?php
+class Mysql
+{
+    private $_serveur = "localhost";
+    private $_login = "root";
+    private $_mdp = "";
+    private $_bdd = "travail_collaboratif";
+    private $_cnx;
 
-
-class Mysql {
-  private $_serveur = "localhost";
-  private $_login;
-  private $_mdp;
-  private $_bdd;
-  private $_cnx;
-
-  public function setServeur($serveur){
-    $this->_serveur = $serveur;
-  }
-
-  public function setLogin($login){
-    $this->_login = $login;
-  }
-
-  public function setMDB($mdb){
-    $this->_mdp = $mdb;
-  }
-
-  public function setDB($db){
-    $this->_bdd = $db;
-  }
-
-  public function getCNX() {
-    return $this->_cnx;
-}
-
-  public function connexion(){
-
-    $this->_cnx = mysqli_connect($this->_serveur, $this->_login, $this->_mdp, $this->_bdd);
-
-    if (!$this->_cnx)
-      exit("Erreur de connexion bdd : " . mysqli_connect_error());
-
-    if (!mysqli_select_db($this->_cnx, $this->_bdd))
-      exit("Erreur : base de données inexistante : " . mysqli_error($this->_cnx));
-
-  }
-
-  public function requete($req) {
-
-    $res = mysqli_query($this->_cnx, $req);
-
-    if (!$res) {
-
-        $error_message = mysqli_error($this->_cnx);
-
-        exit("<pre>Erreur dans la requête [$req] : $error_message</pre>");
+    public function setServeur($serveur) {
+      $this->_serveur = $serveur;
     }
 
-    return $res;
-  }
+    public function setLogin($login) {
+      $this->_login = $login;
+    }
+
+    public function setMDP($mdp){
+      $this->_mdp = $mdp;
+    }
+
+    public function setDB($db) {
+      $this->_bdd = $db;
+    }
+
+    public function getCNX() {
+      return $this->_cnx;
+    }
+
+    public function connexion() {
+      $this->_cnx = new mysqli($this->_serveur, $this->_login, $this->_mdp, $this->_bdd);
+
+      if ($this->_cnx->connect_error)
+        exit("Erreur de connexion bdd : " . $this->_cnx->connect_error);
+    
+    }
+
+    public function requete($req) {
+      $res = $this->_cnx->query($req);
+
+      if (!$res)
+        exit("<pre>Erreur dans la requête [$req] : " . $this->_cnx->error . "</pre>");
+
+      return $res;
+    }
+
 }
