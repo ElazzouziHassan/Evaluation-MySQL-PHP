07f4f7c39f0895fec55b0e0b16542af8d4dabe61
wizardy <wizardhassan99@gmail.com>
2023-10-20 19:25:51 +0100

    form ajouter utilisateur

--- partie-4/projet/admin/utilisateur_ajout.php
+++ partie-4/projet/admin/utilisateur_ajout.php
@@ -1,14 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ajouter nouvel utilisateur</title>
+    <title>Ajouter un utilisateur</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-  <h1>Ajouter nouvel utilisateur</h1>
-  <form  id="form1" name="form1" method="post" action="utilisateur_ajout_action.php">
-    
-  </form>
+    <form id="form1" name="form1" method="post" action="utilisateur_ajout_action.php">
+        <label for="nom">Nom :</label>
+        <input type="text" name="nom" id="nom" required><br><br>
+
+        <label for="prenom">Prénom :</label>
+        <input type="text" name="prenom" id="prenom"><br><br>
+
+        <label for="mail">E-mail :</label>
+        <input type="email" name="mail" id="mail" required><br><br>
+
+        <label for="mdp">Mot de passe :</label>
+        <input type="password" name="mdp" id="mdp" required><br><br>
+
+        <label for="d_naissance">Date de naissance (jj/mm/aaaa) :</label>
+        <input type="text" name="d_naissance" id="d_naissance"><br><br>
+
+        <input type="submit" name="b_ajouter" value="Ajouter">
+    </form>
 </body>
 </html>
