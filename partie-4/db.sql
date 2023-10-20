a633e672b71c95fc12bb23bb49b547691976a8ef
wizardy <wizardhassan99@gmail.com>
2023-10-20 19:02:35 +0100

    updates on db file

--- partie-4/db.sql
+++ partie-4/db.sql
@@ -5,11 +5,14 @@ USE travail_collaboratif;
 
 -- TABLE UTILISATEUR :
 CREATE TABLE `utilisateur` (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  nom VARCHAR(50) NOT NULL,
-  prenom VARCHAR(50) NOT NULL,
-  d_naissance DATE,
-  mail VARCHAR(50) UNIQUE NOT NULL,
-  mdp VARCHAR(15) NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(50) NOT NULL,
+    prenom VARCHAR(50) NOT NULL,
+    d_naissance DATE,
+    mail VARCHAR(50) UNIQUE NOT NULL,
+    mdp VARCHAR(15),
+    CHECK (CHAR_LENGTH(mail) BETWEEN 5 AND 50),
+    CHECK (CHAR_LENGTH(mdp) BETWEEN 4 AND 15)
 );
+
 
