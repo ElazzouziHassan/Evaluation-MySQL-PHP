75e69011e3faac37812a105eff93610e945487a7
wizardy <wizardhassan99@gmail.com>
2023-10-20 20:38:32 +0100

    utilisateur class

--- partie-4/projet/classes/utilisateur.class.php
+++ partie-4/projet/classes/utilisateur.class.php
@@ -1,74 +1,57 @@
 <?php
-class Utilisateur {
-  private $_id;
-  private $_nom;
-  private $_prenom;
-  private $_mail;
-  private $_mdp = "1234";
-  private $_d_naissance;
+class Utilisateur
+{
+    private $_id;
+    private $_nom;
+    private $_prenom;
+    private $_d_naissance;
+    private $_mail;
+    private $_mdp = '1234'; 
 
-  public function setNom($nvNom) {
-      if (strlen($nvNom) == 0) 
+    public function setNom($nom) {
+      if ($nom === null || strlen($nom) == 0)
         exit("Utilisateur : le nom est obligatoire");
+
+      $this->_nom = $nom;
+    }
+
+    public function setPrenom($prenom) {
+        $this->_prenom = $prenom;
+    }
+
+    public function setMail($mail) {
+        if (strlen($mail) < 5 || strlen($mail) > 50) 
+          exit("Utilisateur : la taille du mail doit être entre 5 et 50 caractères");
+        
+        $this->_mail = $mail;
+    }
+
+    public function setMDP($mdp) {
+        if (strlen($mdp) < 4 || strlen($mdp) > 15)
+          exit("Utilisateur : le mdp doit être compris entre 4 et 15 caractères");
       
-      $this->_nom = $nvNom;
-  }
+        $this->_mdp = $mdp;
+    }
 
-  public function setPrenom($nvPrenom) {
-    if (strlen($nvPrenom) == 0) 
-        exit("Utilisateur : le nom est obligatoire");  
+    public function setDNaissance($dateNaissance) {
+        $this->_d_naissance = $dateNaissance;
+    }
 
-    $this->_prenom = $nvPrenom;
-  }
+    public function setID($id) {
+        $this->_id = $id;
+    }
 
-  public function setMail($nvMail) {
-    if (strlen($nvMail) < 5 or strlen($nvMail) > 50) 
-      exit("Non Valide mail !"); 
+    public function enregistrer(Mysql $bdd) {
+      $req = "INSERT INTO utilisateur (id, nom, prenom, d_naissance, mail, mdp) 
+              VALUES (null, '$this->_nom', '$this->_prenom', '$this->_d_naissance', '$this->_mail', '$this->_mdp')";
 
-    $this->_mail = $nvMail;
-  }
-
-  public function setMDP($nvMDP) {
-      if (empty($nvMDP)) {
-          $nvMDP = "1234";
-      }
-      if (strlen($nvMDP) < 4 || strlen($nvMDP) > 15) 
-        exit("Utilisateur : le mdp doit être compris entre 4 et 15 caractères");
-
-      $this->_mdp = $nvMDP;
-  }
-
-  public function setDNaissance($nvDN) {
-      $this->_d_naissance = $nvDN;
-  }
-
-  public function setID($nvID) {
-      $this->_id = $nvID;
-  }
-
-  public function enregistrer(Mysql $bdd){
-    $req = `
-      INSERT INTO 
-        utilisateur (id, nom, prenom, d_naissance, mail, mdp) 
-        VALUES (
-          null, 
-          '$this->_nom', 
-          '$this->_prenom', 
-          '$this->_d_naissance', 
-          '$this->_mail', 
-          '$this->_mdp'
-        )
-      `;
-    
-    return $bdd->requete($req);
-  }
-
-  public function supprimer(Mysql $bdd){
-
-    if ($this->_id) {
-      $req = "DELETE FROM utilisateur WHERE id = $this->_id";
       return $bdd->requete($req);
     }
-  }
 
-}
+    public function supprimer(Mysql $bdd) {
+      if ($this->_id) {
+        $req = "DELETE FROM utilisateur WHERE id = $this->_id";
+        return $bdd->requete($req);
+      }
+    }
+  }
