4696aac4ede4d5d317a0957a8da565576ed77f48
wizardy <wizardhassan99@gmail.com>
2023-10-20 20:38:47 +0100

    connect file

--- partie-4/projet/inc/connection.php
+++ partie-4/projet/inc/connection.php
@@ -1,13 +1,9 @@
 <?php
-
-include("..\classes\mysql.class.php");
+include("../classes/mysql.class.php"); 
 
 $bdd = new Mysql();
 $bdd->setServeur("localhost");
-$bdd->setLogin("root");
-$bdd-> setMDB("");
-$bdd->setDB("travail_collaboratif");
+$bdd->setLogin("root"); 
+$bdd->setMDP(""); 
+$bdd->setDB("travail_collaboratif"); 
 $bdd->connexion();
-
-
-
