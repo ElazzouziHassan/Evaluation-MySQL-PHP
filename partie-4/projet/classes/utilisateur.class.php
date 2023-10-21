f582f371c41d346b10001a84dcaad7f696720912
wizardy <wizardhassan99@gmail.com>
2023-10-21 23:28:53 +0100

    commit

--- partie-4/projet/classes/utilisateur.class.php
+++ partie-4/projet/classes/utilisateur.class.php
@@ -35,26 +35,29 @@ class Utilisateur {
   
   // !Setters :
   public function setNom($nom) {
-    if ($nom === null || strlen($nom) == 0)
-    exit("Utilisateur : le nom est obligatoire");
+    if (strlen($nom) == 0)
+      exit("Utilisateur : le nom est obligatoire");
     
     $this->_nom = $nom;
   }
   
   public function setPrenom($prenom) {
+    if (strlen($prenom) == 0)
+      exit("Utilisateur : le nom est obligatoire");
+
     $this->_prenom = $prenom;
   }
   
   public function setMail($mail) {
     if (strlen($mail) < 5 || strlen($mail) > 50) 
-    exit("Utilisateur : la taille du mail doit être entre 5 et 50 caractères");
+      exit("Utilisateur : la taille du mail doit être entre 5 et 50 caractères");
     
     $this->_mail = $mail;
   }
   
   public function setMDP($mdp) {
     if (strlen($mdp) < 4 || strlen($mdp) > 15)
-    exit("Utilisateur : le mdp doit être compris entre 4 et 15 caractères");
+      exit("Utilisateur : le mdp doit être compris entre 4 et 15 caractères");
     
     $this->_mdp = $mdp;
   }
@@ -77,8 +80,7 @@ class Utilisateur {
 
   public function supprimer(Mysql $bdd) {
     if ($this->_id) {
-
-    $req = "DELETE FROM utilisateur WHERE id = $this->_id";
+      $req = "DELETE FROM utilisateur WHERE id = $this->_id";
       return $bdd->requete($req);
     }
   }
